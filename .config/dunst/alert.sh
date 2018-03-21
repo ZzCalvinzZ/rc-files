1d4bec185ccb02a8332d673e1052d2b4dcc87b2b
Calvin Collins <calvinkcollins@gmail.com>
2018-03-21 18:32:46 -0400

    random notification sounds

--- .config/dunst/alert.sh
+++ .config/dunst/alert.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-paplay ~/.config/sounds/link_whistle.wav
+SOUNDLIST=( \
+    "link_whistle.wav" \
+    "link_fairy.wav" \
+    "link_rupee.wav" \
+)
+
+SOUND=${SOUNDLIST[ $(( RANDOM % ${#SOUNDLIST[@]} )) ]}
+
+paplay ~/.config/sounds/$SOUND
