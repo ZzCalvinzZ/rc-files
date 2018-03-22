014e3cbdb6be01e7ac174db01bef32a0c1b80742
Calvin Collins <calvinkcollins@gmail.com>
2018-03-22 14:46:04 -0400

    notifications more advanced different sounds

--- .config/dunst/alert.sh
+++ .config/dunst/alert.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
-SOUNDLIST=( \
-    "link_whistle.wav" \
-    "link_fairy.wav" \
-    "link_rupee.wav" \
+APP=$1
+SUMMARY=$2
+DESC=$3
+IDUNNO=$4
+URGENCY=$5
+
+NORMAL_SOUNDLIST=( \
+    "normal/link_whistle.wav" \
+    "normal/link_fairy.wav" \
+    "normal/link_rupee.wav" \
+    "normal/smb_coin.wav" \
+)
+CRIT_SOUNDLIST=( \
+    "critical/link_menu_erase.wav" \
 )
 
-SOUND=${SOUNDLIST[ $(( RANDOM % ${#SOUNDLIST[@]} )) ]}
+if [[ $URGENCY = "LOW" ]]; then
+	:
+elif [[ $URGENCY = "NORMAL" ]]; then
+	if [[ $APP = "Kitty" ]]; then
+		SOUND="normal/link_get_heart.wav"
+	else
+		SOUND=${NORMAL_SOUNDLIST[ $(( RANDOM % ${#NORMAL_SOUNDLIST[@]} )) ]}
+	fi
+	#statements
+elif [[ $URGENCY = "CRITICAL" ]]; then
+	if [[ $APP = "Kitty" ]]; then
+		SOUND="critical/link_error.wav"
+	else
+		SOUND=${CRIT_SOUNDLIST[ $(( RANDOM % ${#CRIT_SOUNDLIST[@]} )) ]}
+	fi
+fi
 
-paplay ~/.config/sounds/$SOUND
+if [[ -n $SOUND ]]; then
+	paplay ~/.config/sounds/$SOUND
+fi
